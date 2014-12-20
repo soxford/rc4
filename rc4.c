81875c7fe56723810d6f99abdb13fbabb5fbcdbb
Sox Ford <soxford1379@gmail.com>
2014-12-20 12:57:07 +0000

    initial commit

---
+++ rc4.c
@@ -0,0 +1,15 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Title: rc4
+ * Author: Simon Campbell, soxford1379@gmail.com
+ * Description: code for the rc4 stream cipher
+ * License: GPL
+ * Date: 20th December 2014
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+      
+   return 0;
+}
