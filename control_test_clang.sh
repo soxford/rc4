95624e334772e3f3a8cfabc21923dfeeb3a510a7
Sox Ford <soxford1379@gmail.com>
2015-05-20 15:04:38 +0100

    data file name update

--- control_test_clang.sh
+++ control_test_clang.sh
@@ -1,6 +1,6 @@
 clang++ -std=c++11 -stdlib=libc++ -Os ./tests/correctness\ tests/rc4Test.cpp -o test1
 ./test1
 clang++ -std=c++11 -stdlib=libc++ -Os ./tests/optimControlTest.cpp -o test2
-./test2 ./HOptimisationTests.txt
+./test2 ./HomeOptimisationTests.txt
 rm ./test1
 rm ./test2
