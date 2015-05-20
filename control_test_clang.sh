6881584471d62d250ef33e7faa2674bdd6086686
Sox Ford <soxford1379@gmail.com>
2015-05-20 14:55:59 +0100

    Home Optimization Tests with clang

---
+++ control_test_clang.sh
@@ -0,0 +1,6 @@
+clang++ -std=c++11 -stdlib=libc++ -Os ./tests/correctness\ tests/rc4Test.cpp -o test1
+./test1
+clang++ -std=c++11 -stdlib=libc++ -Os ./tests/stackAllocatedControlTest.cpp -o test2
+./test2 ./HomeOptimisationTests.txt
+rm ./test1
+rm ./test2
