878557f3bc26a942038b162c72ca464f9b2e0cd3
Simon Campbell <soxford1379@gmail.com>
2015-05-21 14:20:34 +0100

    naive threaded control test

---
+++ tests/threadedControlTest.cpp
@@ -0,0 +1,120 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Title: controlTest.cpp 
+ * Author: Simon Campbell, <simonhmcampbell@gmail.com>
+ * Description: A control test for comparison to variants for the identification of bottlenecks
+ * License: GPL
+ * Date: April 2015
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <thread>
+#include "../MT19937_RandomSource.cpp"
+
+//variables used in documenting the test
+const char* TEST_NAME = "Control Test with threaded execution of data generation";
+const char* FIELDS = "Number of RC4 Streams & Time Spent Initializing and Generating RC4 Streams (s)";
+int STREAM_OUTPUT_LENGTH = 257; //IF YOU CHANGE THIS THEN MAKE SURE YOU USE THE RIGHT PRGOutput METHOD FOR THE REQUIRED LENGTH OF OUTPUT
+
+int MAX_LOOPCOUNT = 1000000;
+int NUM_THREADS = 2;
+using namespace std;
+void call_from_thread(int tid) {
+
+   string fileName = "ThreadedTestData" + to_string(tid);
+
+   //open log file for appending data
+   ofstream logFile ;
+   logFile.open(fileName, ofstream::app);
+   if(!logFile.is_open()){
+      cout << "Error: failed to open file named " << fileName << endl;
+      return;
+   }
+   
+   //Output details of the attempted test to the logfile
+   logFile << endl << "===============================================================" << endl << endl;
+   logFile << "Test Name: " << TEST_NAME << endl;
+   time_t t = time(0);   // get time now
+   struct tm * now = localtime( & t );
+   logFile << "Date/Time: "   << (now->tm_year + 1900) << '-' 
+                              << (now->tm_mon + 1) << '-'
+                              <<  now->tm_mday << " "
+                              <<  now->tm_hour << ":"
+                              <<  now->tm_min
+                              << endl;
+   logFile << "Length of each RC4 stream in bytes: " << STREAM_OUTPUT_LENGTH << endl;
+   //allocate space to hold table of results
+   long histograms[STREAM_OUTPUT_LENGTH][RC4Stream::PERMUTATION_ARRAY_LENGTH]; //TODO consider potential for cache misses with this data structure, perhaps buffer output?
+   for (int i = 0; i < STREAM_OUTPUT_LENGTH; i++) {
+      for (int j = 0; j < RC4Stream::PERMUTATION_ARRAY_LENGTH; j++) {
+         histograms[i][j] = 0;
+      }
+   }
+
+   //assign space for the key;
+   RC4Stream::Key key;
+
+   //allocate space for the RC4 stream
+   RC4Stream rc4Stream;
+
+   //initialize Random Number Generation algorithm 
+   RC4Stream::Key::RandomSource randomSource;
+
+   randomSource.initializeRandomNoGen();
+
+   //variables for measuring clock usage
+   clock_t begin, end;
+   double time_spent;
+   uint8_t outputs[STREAM_OUTPUT_LENGTH];
+ 
+   //record test data
+   logFile << "Test Data:" << endl;
+   logFile << FIELDS << endl;
+
+   //try various loop counts to compare speed
+   for (int loopcount = 1; loopcount <= MAX_LOOPCOUNT; loopcount*=10) {
+      begin = clock();
+
+      //loop to generate multiple stream outputs
+      for (int i = 0; i < loopcount; i++) {
+        
+        //random key generation
+        randomSource.selectRandomKey(key);
+         
+        //rekey
+        rc4Stream.keySchedule(key);
+        //get output from the RC4 stream 
+ 	rc4Stream.PRGOutputFirst257BytesToArray(outputs);
+        //collect output in histogram counters
+        for (int j = 0; j < STREAM_OUTPUT_LENGTH; j++) {
+           histograms[j][outputs[j]]++; //increment the relevant histogram count
+        }
+      }
+
+      end = clock();
+      time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+      //report time_spent
+      logFile << loopcount << " & " << time_spent << endl;
+   }
+
+   //close the file and return
+   logFile.close();
+}
+
+//MAIN
+int main(int argc, const char *argv[])
+{
+   thread t[NUM_THREADS];
+   
+   for (int i = 0; i < NUM_THREADS; i++) {
+      t[i] = thread(call_from_thread, i);
+   }
+
+   for (int i = 0; i < NUM_THREADS; i++){
+      t[i].join();
+   }
+   return 0;
+}
+
