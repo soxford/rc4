4483fde723029898b1c8fb8125b48e3586c59be4
Simon Campbell <soxford1379@gmail.com>
2015-07-03 11:06:44 +0100

    Guessing Entropy Data for single byte first 257 positions

--- data_analysis/SingleByteSyntheticSamplingAnalysis.m
+++ data_analysis/SingleByteSyntheticSamplingAnalysis.m
@@ -1,61 +1,66 @@
-%Code to generate synthetic random samples of single byte rc4 data of varying
-%sizes. This data is then used to measure ease of recovering message bytes
-%using different attack methodologies.
+%Code to generate synthetic random samples of single byte rc4 data of
+%varying sizes. This data is then used to measure the average number of
+%guesses to get the correct message byte by matching the peak value in
+%order
 format longe;
-%output byte to check
-outputByteNo = 3;
-%number of synthetic experiments
-samplecount = 12;
-%get teh index of the maximum peak in the distribution of the first byte
-[~, expInd] = max(experimentalProbs(outputByteNo, :));
-%array for collecting results for each synthetic experiment
-successRate = ones(samplecount, 1);
+% Number of outcomes for each byte position, required size of the noise
+% vector = k
+% Number of byte positions = bytePositionCount
+[bytePositionCount, k] = size(experimentalProbs);
 %base power for each sample size
-basepower = 10;
+basepower = 9;
+%number of sample sizes tested
+samplecount = 37;
+%get the index of the maximum peak in the distribution of the first byte
+%(i.e. the index that would correctly identify the message byte if we
+%looked for the maximum peak to break the cipher
+[~, correctValIndices] = max(experimentalProbs, [], 2);
+%matrix for collecting results for each synthetic experiment
+averageNoOfGuesses = ones(samplecount, bytePositionCount);
+
+%Number of synthesised samplings overwhich we will average performance
+noOfSynthesisedSamplings = 10000;
 for i = 1:samplecount
     power = basepower + i;
     simulatedNoOfSamples = 2^power;
-    %byte number and value specific standard deviations
-    %sigmas = sqrt(experimentalProbs .* (1 - experimentalProbs) / simulatedNoOfSamples)
-    k = 256; % Number of outcomes, required size of the noise vector
-    %uniform distribution standard deviation
+    
+    %uniform distribution standard deviation (approximation of the standard
+    %deviation for each experimental probability observation)
     sigma = sqrt((1/256)*(1-1/256)/simulatedNoOfSamples);
-
+    
+    %Generate noOfSynthesisedSamplings normally distributed vectors of
+    %noise of size 256
     Q = null(ones(1,k));
-    %X = sqrt(k/(k-1))*(((sigmas(1,:).')).*(Q*randn(k-1,simulatedNoOfSamples)));
-    %TODO Trying to get different standard deviations accross the range of values.
-    %Currently using a constant standard deviation based on the sample size and
-    %a uniform distribution of the 256 byte values:
-    X = sqrt(k/(k-1))*sigma*Q*randn(k-1,simulatedNoOfSamples);
-    %X is a 256 by simulatedNoOfSamples matrix that is meant to have each
-    %column summing to one and each row being samples from a normal
+     X = sqrt(k/(k-1))*sigma*Q*randn(k-1,noOfSynthesisedSamplings);
+    %X is a 256 by noOfSythesisedSamplings matrix that is meant to have each
+    %column summing to 0 and each row being samples from a normal
     %distribution with 0 mean and the appropriate standard deviation for the
-    %chosen byte value for the selected byte position
-    %Should we try and specify the accurate sigma based on the best available
-    %data, or should we treat sigma as if all byte values have the same standard deviation?
+    %chosen byte value for the selected byte position (infact using uniform
+    %sigma for all values as an approximation)
     
-    %Find the maximum values for each column and get their row number in
-    %Indices
-    for col = 1: simulatedNoOfSamples
-        X(:, col) = X(:, col) +  experimentalProbs(outputByteNo , :).';
+    %Count how many guesses would be required to find the correct value by
+    %searching through the peaks in order (assume that if multiple values
+    %have the same value then all of theme are tried)
+    sumOfGuessesRequired = zeros(1, bytePositionCount);
+    correctDistributions = experimentalProbs.';
+    for col = 1:noOfSynthesisedSamplings
+        %Calculate the synthesised sampled distribution
+        sythesisedSample = repmat(X(:, col), 1, bytePositionCount) + correctDistributions;
+        %count the number of elements of this sample which are bigger than
+        %the value that is observed for the correct index for each
+        %byteValue
+        for bytePosition = 1:bytePositionCount
+            bytePositionSynthesisedSample = sythesisedSample(:, bytePosition);
+            [m, n] = size(bytePositionSynthesisedSample(bytePositionSynthesisedSample >= bytePositionSynthesisedSample(correctValIndices(bytePosition,1), 1)));
+            sumOfGuessesRequired(1, bytePosition) = sumOfGuessesRequired(1, bytePosition) + m;
+        end
     end
-     %free up memory
-    [~, Indices] = max(X, [], 1);
-    %free up memory
-    clearvars X;
-    %Count how many samples agree on the maximum value with the correct
-    %value
-    [m, n] = size(Indices(Indices == expInd));
-    clearvars Indices;
-    
-    %output the percentage observations which gave the correct index for
-    %the maximum (i.e. correctly identified the message byte using the
-    %maximum peak method)
-    successRate(i, 1) = n*100/simulatedNoOfSamples;
+    %output the average number of guesses that the samplings would have
+    %required to guess the correct message byte vlaue for this byte value
+    averageNoOfGuesses(i, :) = sumOfGuessesRequired/noOfSynthesisedSamplings;
 end
 
     xVals = basepower+1 : basepower+samplecount;
-    plot(xVals,successRate);
-    % Test
-    %sum(X(:, 1:10), 1)
-    %std(X')
+    %plot the average number of guesses against the sample size
+    plot(xVals,averageNoOfGuesses(:,3));
+
