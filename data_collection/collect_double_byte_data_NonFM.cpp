91047cd07f5a96efd37dc42e468ce70a2e7e6995
Simon Campbell <soxford1379@gmail.com>
2015-07-19 20:38:34 +0100

    non FM double byte arcus code ready for testing

--- data_collection/collect_double_byte_data_NonFM.cpp
+++ data_collection/collect_double_byte_data_NonFM.cpp
@@ -2,7 +2,7 @@
  * Title: collect_double_byte_data.cpp
  * Author: Simon Campbell, simonhmcampbell@gmail.com
  * Description: Will produce histograms indexed by the internal state of the RC4 Stream (_i) and then indexed by the value of the the byte output and the value of the successor byte. The biases being investigated are longer term and require _permutationArray and _j to be "random" so we will ignore the first 2^10 bytes per AF:13. The quantity of bytes required is tbd but the requirement will be to get a long stream (perhaps 2^40 bytes for each key as per AF:13)
- * Date: June 2015
+ * Date: July 2015
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #include <iostream>
 #include <string>
@@ -80,9 +80,9 @@ int main(int argc, char *argv[])
         //initialize the histogram and stream/key variables
         for (int i = 0; i < HISTOGRAM_COUNT; i++) {
           for (int j = 0; j < RC4Stream::PERMUTATION_ARRAY_LENGTH; j++) {
-	    for (int k = 0; k < RC4Stream::PERMUTATION_ARRAY_LENGTH; k++) {
+	        for (int k = 0; k < RC4Stream::PERMUTATION_ARRAY_LENGTH; k++) {
               histograms[i][j][k] = 0;
-	    }
+	        }
           }
         }
 
@@ -118,22 +118,22 @@ int main(int argc, char *argv[])
 
 	    //discard the first 2^10 output bytes as per AF:13
 	    for (int skipPosition = 0; skipPosition < (1 << 10) ; skipPosition++) {
-		rc4Stream.PRGRound();
-            }
+		    rc4Stream.PRGRound();
+        }
 	    //loop to generate the output stream and collect results
 	    first_byte = rc4Stream.PRGRound(); //loop assumes that the first byte has been collected
 	      
 	    for (int64_t i = 0; i < BATCH_SIZE ; i++) {
 		second_byte = rc4Stream.PRGRound();
 		//map indices to histograms selectively as follows
-		if(first_byte == 0x00){//collect data of the form (i, 0, k) in histograms[0][i][k]
+		if(first_byte == 0x01){//collect data of the form (i, 1, k) in histograms[0][i][k]
 			 histograms[0][i & MASK_8][second_byte]++; //increment the relevant histogram count
-		} else if (first_byte == 0xff) { //(i, 255, k) -> histograms[1][i][k]
+		} else if (first_byte == 0xfe) { //(i, 254, k) -> histograms[1][i][k]
 			 histograms[1][i & MASK_8][second_byte]++; //increment the relevant histogram count
-		} else if (second_byte == 0xff) { //(i, j, 255) -> histograms[2][i][j]
-			 histograms[2][i & MASK_8][first_byte]++; //increment the relevant histogram count
-		} else { //all other (i, j, k) -> histograms[3][j][k]
-			 histograms[3][first_byte][second_byte]++;
+		} else if (first_byte == 0x80) { //(i, 128, k) -> histograms[3][i][k]
+			 histograms[2][i & MASK_8][second_byte]++;
+		} else if (second_byte == 0xfe) { //(i, j, 254) -> histograms[2][i][j]
+			 histograms[3][i & MASK_8][first_byte]++; //increment the relevant histogram count
 		}
 		first_byte = second_byte; //prepare for next loop
 	    }
@@ -171,8 +171,8 @@ void dumpDoubleByteData(std::ostream &outStream, uint64_t histograms[][RC4Stream
 		      << "% The histograms count occurrences of events of the form (i, j, k) = (i, S_i, S_i+1) where i is the _i partial-state variable for rc4 and S_i is an output from rc4 in that state and S_i+1 is the subsequent output" << endl << endl;
 
 	    //First Histogram
-	    outStream << "% The entry histogrami0k[i][k] is the count of occurences of (i, 0x00, k)  " << endl
-            	      << "histogram_i0k_id" << proc_id << " = [ " << endl;
+	    outStream << "% The entry histogrami1k[i][k] is the count of occurences of (i, 0x01, k)  " << endl
+            	      << "histogram_i1k_id" << proc_id << " = [ " << endl;
             for(int i = 0; i < RC4Stream::PERMUTATION_ARRAY_LENGTH; i++) {
                 outStream << '\t';
                 for(int k = 0; k < RC4Stream::PERMUTATION_ARRAY_LENGTH; k++){
@@ -183,8 +183,8 @@ void dumpDoubleByteData(std::ostream &outStream, uint64_t histograms[][RC4Stream
             outStream << "\t];" << endl;
 	
 	    //Second Histogram
-	    outStream << endl << "% The entry histogrami255k[i][k] is the count of occurences of (i, 0xff, k)  " << endl
-            	      << "histogram_i255k_id" << proc_id << " = [ " << endl;
+	    outStream << endl << "% The entry histogrami254k[i][k] is the count of occurences of (i, 0xfe, k)  " << endl
+            	      << "histogram_i254k_id" << proc_id << " = [ " << endl;
             for(int i = 0; i < RC4Stream::PERMUTATION_ARRAY_LENGTH; i++) {
                 outStream << '\t';
                 for(int k = 0; k < RC4Stream::PERMUTATION_ARRAY_LENGTH; k++){
@@ -195,24 +195,24 @@ void dumpDoubleByteData(std::ostream &outStream, uint64_t histograms[][RC4Stream
             outStream << "\t];" << endl;
 
 	    //Third Histogram
-	    outStream << endl << "% The entry histogramij255[i][j] is the count of occurences of (i, j, 0xff)  " << endl
-            	      << "histogram_ij255_id" << proc_id << " = [ " << endl;
-            for(int i = 0; i < RC4Stream::PERMUTATION_ARRAY_LENGTH; i++) {
+	    outStream << endl << "% The entry histogrami128k[i][k] is the count of occurences of (i, 0x80, k)" << endl
+            	      << "histogrami128k_id" << proc_id << " = [ " << endl;
+            for(int j = 0; j < RC4Stream::PERMUTATION_ARRAY_LENGTH; j++) {
                 outStream << '\t';
-                for(int j = 0; j < RC4Stream::PERMUTATION_ARRAY_LENGTH; j++){
-                    outStream << histograms[2][i][j] << ' ';   
+                for(int k = 0; k < RC4Stream::PERMUTATION_ARRAY_LENGTH; k++){
+                    outStream << histograms[2][j][k] << ' ';   
                 }
                 outStream << ';' << endl;
             }
             outStream << "\t];" << endl;
 
 	    //Final/Fourth Histogram
-	    outStream << endl << "% The entry histogramDefault[j][k] is the count of occurences of (i, j, k) for all i, which were not counted in any other histogram" << endl
-            	      << "histogramDefault_id" << proc_id << " = [ " << endl;
-            for(int j = 0; j < RC4Stream::PERMUTATION_ARRAY_LENGTH; j++) {
+	    outStream << endl << "% The entry histogramij254[i][j] is the count of occurences of (i, j, 0xfe)  " << endl
+            	      << "histogram_ij254_id" << proc_id << " = [ " << endl;
+            for(int i = 0; i < RC4Stream::PERMUTATION_ARRAY_LENGTH; i++) {
                 outStream << '\t';
-                for(int k = 0; k < RC4Stream::PERMUTATION_ARRAY_LENGTH; k++){
-                    outStream << histograms[3][j][k] << ' ';   
+                for(int j = 0; j < RC4Stream::PERMUTATION_ARRAY_LENGTH; j++){
+                    outStream << histograms[3][i][j] << ' ';   
                 }
                 outStream << ';' << endl;
             }
@@ -223,31 +223,14 @@ void dumpDoubleByteData(std::ostream &outStream, uint64_t histograms[][RC4Stream
             outStream << endl << "% The total Number of bytes of data collected for each index i" << endl 
                               <<"numberOfBytesCollectedPerI = " << numberOfBatches << '*' << BATCH_SIZE << "/" << RC4Stream::PERMUTATION_ARRAY_LENGTH << ';' << endl
 
-	     		      //TODO double check supplementation of histograms here
-			      //Ensure histogramij255 is complete
+			      //Ensure histogramij254 is complete
 		      << endl << "% Ensure that the histograms contain all required data by supplementing them from each other where necessary" << endl
-			      << "% Supplement histogram_ij255_id" << proc_id << " from the last columns of i0k and i255k" << endl
-			      << "histogram_ij255_id" << proc_id << "(:, 1) = histogram_i0k_id" << proc_id << "(:, 256);" << endl
-			      << "histogram_ij255_id" << proc_id << "(:, 256) = histogram_i255k_id" << proc_id << "(:, 256);" << endl << endl
+			      << "% Supplement histogram_ij254_id" << proc_id << " from the last but one columns of i1k, i254k, and i128k" << endl
+			      << "histogram_ij254_id" << proc_id << "(:, 2) = histogram_i1k_id" << proc_id << "(:, 255);" << endl
+			      << "histogram_ij254_id" << proc_id << "(:, 255) = histogram_i254k_id" << proc_id << "(:, 255);" << endl
+			      << "histogram_ij254_id" << proc_id << "(:, 129) = histogram_i128k_id" << proc_id << "(:, 255);" << endl << endl
 
-			      //Ensure experimentalProbsDefault is complete
-			      << "% Supplement histogramDefault from the previous three histograms" << endl
-			      << "histogramDefault_id" << proc_id << "(1, :) = sum(histogram_i0k_id" << proc_id << ");" << endl
-			      << "histogramDefault_id" << proc_id << "(256, :) = sum(histogram_i255k_id" << proc_id << ");" << endl
-			      << "histogramDefault_id" << proc_id << "(2:255, 256) = (sum(histogram_ij255_id" << proc_id << "(:, 2:255))).\';" << endl
+             	  << "% Set the format to longe to capture the differences in the experimental probabilities" << endl
+                  << "format longe" << endl;
 
-             	      << endl << "% Set the format to longe to capture the differences in the experimental probabilities" << endl
-                              << "format longe" << endl
-             	      << endl << "% The experimental conditional probability values observed for the output double bytes" << endl
-                      	      << "experimentalCondProbs_i0k_id" << proc_id << " = histogram_i0k_id" << proc_id << " ./ numberOfBytesCollectedPerI;" << endl
-                      	      << "experimentalCondProbs_i255k_id" << proc_id << " = histogram_i255k_id" << proc_id << " ./ numberOfBytesCollectedPerI;" << endl
-                      	      << "experimentalCondProbs_ij255_id" << proc_id << " = histogram_ij255_id" << proc_id << " ./ numberOfBytesCollectedPerI;" << endl
-                      	      << "experimentalProbsDefault_id" << proc_id << " = histogramDefault_id" << proc_id << " ./ (numberOfBytesCollectedPerI * " << RC4Stream::PERMUTATION_ARRAY_LENGTH << ");" << endl;
-
-	    //print intstructions on using the data histograms
-	    outStream << endl << "%Print out instructions for using the data histograms/probabilities" << endl
-		      	      << "fprintf(\'experimentalCondProbs_i0k_id" << proc_id << "(i+1,k+1) contains the experimental probability conditioned on the value of i for (i, 0x00, k), i.e. the event the the rc4 stream output 0x00 followed by k given initial partial state i.\\n\');" << endl
-		      	      << "fprintf(\'experimentalCondProbs_i255k_id" << proc_id << "(i+1, k+1) contains the experimental probability conditioned on the value of i for (i, 0xff, k), i.e. the event the the rc4 stream output 0xff followed by k given initial partial state i.\\n\');" << endl
-		      	      << "fprintf(\'experimentalCondProbs_ij255_id" << proc_id << "(i+1, j+1) contains the experimental probability conditioned on the value of i for (i, j, 0xff), i.e. the event the the rc4 stream output j followed by 0xff given initial partial state i.\\n\');" << endl
-		      	      << "fprintf(\'experimentalProbsDefault_id" << proc_id << "(j+1, k+1) contains the experimental probability (NOT conditioned on the value of i) for (i, j, k) for all i. i.e. the event the the rc4 stream output j followed by k regardless of the initial partial state i.\\n\');";
 }
