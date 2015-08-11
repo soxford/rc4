9a404a06f70f5cea316545601aa4d09375c7605a
Sox Ford <soxford1379@gmail.com>
2015-08-11 11:34:30 +0100

    tidy guessing entropy max peak code

--- data_analysis/GuessingEntropyWithMaxPeak.m
+++ data_analysis/GuessingEntropyWithMaxPeak.m
@@ -1,49 +1,53 @@
-%Code to generate synthetic random samples of single byte rc4 data of
-%varying sizes. This data is then used to measure the average number of
-%guesses to get the correct message byte by matching the peak value in
-%order
-function [logSampleSizes,averageNoOfGuesses] = GuessingEntropyWithMaxPeak(experimentalProbs, samplecount)% Number of outcomes for each byte position, required size of the noise
-% vector = k
+%Code to generate 15000 synthetic random samples for each ciphertext count from 2^{8+1} up to 
+%2^{8+samplecount}. This data is then used to measure the average number of
+%guesses to get the correct message byte using the "max-peak" attack. 
+%The inputs are the a matrix of distributions for each byte position under 
+%consideration, which are taken to be the correct RC4 output distributions,
+%and a samplecount, which indicates the number of different ciphertext counts to be
+%considered. 
+%The ouputs are the log2 of the ciphertext counts tried and the average 
+%number of guesses required for each ciphertext count value and output
+%position.
+function [logCiphertextCounts,averageNoOfGuesses] = GuessingEntropyWithMaxPeak(obsSingleByteDistributions, samplecount)
+%Number of synthesised samplings overwhich we will average performance
+noOfSynthesisedSamplings = 15000;
+%get the (assumed) correct distributions for each byte position
+correctDistributions = obsSingleByteDistributions.';
 % Number of byte positions = bytePositionCount
-[bytePositionCount, k] = size(experimentalProbs);
-%base power for each sample size
+[bytePositionCount, k] = size(obsSingleByteDistributions);
+%base power for each sample size (ciphertext counts of less than 2^8 are
+%likely to be insufficient).
 basepower = 8;
-%number of sample sizes tested
-%samplecount = 37;
 %get the index of the maximum peak in the distribution of the first byte
 %(i.e. the index that would correctly identify the message byte if we
 %looked for the maximum peak to break the cipher
-[~, correctValIndices] = max(experimentalProbs, [], 2);
+[~, correctValIndices] = max(obsSingleByteDistributions, [], 2);
 %matrix for collecting results for each synthetic experiment
 averageNoOfGuesses = ones(samplecount, bytePositionCount);
-
-%Number of synthesised samplings overwhich we will average performance
-noOfSynthesisedSamplings = 15000;
+%loop to simulate samplings for each ciphertext count value
 for i = 1:samplecount
     power = basepower + i;
-    simulatedNoOfSamples = 2^power;
+    simulatedNoOfCipherTextSamples = 2^power;
     
-    %uniform distribution standard deviation (approximation of the standard
-    %deviation for each experimental probability observation)
-    sigma = sqrt((1/256)*(1-1/256)/simulatedNoOfSamples);
+    %uniform distribution standard deviation (used as approximation of the 
+    %standard deviation for each experimental probability observation)
+    sigma = sqrt((1/256)*(1-1/256)/simulatedNoOfCipherTextSamples);
     
     %Generate noOfSynthesisedSamplings normally distributed vectors of
     %noise of size 256
     Q = null(ones(1,k));
      X = sqrt(k/(k-1))*sigma*Q*randn(k-1,noOfSynthesisedSamplings);
-    %X is a 256 by noOfSythesisedSamplings matrix that is meant to have each
+    %X is a 256 by noOfSythesisedSamplings matrix that has each
     %column summing to 0 and each row being samples from a normal
-    %distribution with 0 mean and the appropriate standard deviation for the
-    %chosen byte value for the selected byte position (infact using uniform
-    %sigma for all values as an approximation)
+    %distribution with 0 mean and standard deviation sigma.
     
-    %Count how many guesses would be required to find the correct value by
-    %searching through the peaks in order (assume that if multiple values
-    %have the same value then all of theme are tried)
+    %Count how many guesses would be required to find the correct value of 
+    %m_r (which is 0 in this case) by searching through the peaks in order
+    %(assume that if multiple candidates have the same value then all of theme 
+    % are tried to give a worst case analysis)
     sumOfGuessesRequired = zeros(1, bytePositionCount);
-    correctDistributions = experimentalProbs.';
     for col = 1:noOfSynthesisedSamplings
-        %Calculate the synthesised sampled distribution
+        %Calculate the synthesised sampled distributions
         sythesisedSample = repmat(X(:, col), 1, bytePositionCount) + correctDistributions;
         %count the number of elements of this sample which are bigger than
         %the value that is observed for the correct index for each
@@ -59,6 +63,6 @@ for i = 1:samplecount
     averageNoOfGuesses(i, :) = sumOfGuessesRequired/noOfSynthesisedSamplings;
 end
 
-    logSampleSizes = basepower+1 : basepower+samplecount;
+    logCiphertextCounts = basepower+1 : basepower+samplecount;
 end
 
---
+++ tests/arcus_tests/double_byte_arcus/histogram_size_tests/double_byte_hist_size_timing_tests.m
@@ -0,0 +1,22 @@
+%Plot the timings of the test code for different histogram sizes
+load('ArcusTestData1.mat')
+
+    %plot the X values against the Timing Values
+    plot(X, Y, '--s');
+    
+    % For R2014a and earlier:
+    ax = gca;
+    %limit the x axis
+    xlim([-1 8]);
+    ylim([0 8]);
+    %set the x axis tick marks
+    set(ax, 'Xtick', X);
+    set(ax, 'XTickLabel', 2 .^ X);
+    %turn on the y grid lines
+    set(ax,'ygrid','on');
+    set(ax,'xgrid','on');
+    %keep the box off
+    set(ax, 'box', 'off');
+    xlabel('Histogram[x][256][256] x-Dimension size (log scale)');
+    ylabel('Test Time (s)');
+    print -depsc ArcusTestData1
